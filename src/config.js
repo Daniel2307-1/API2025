77a4cb2da5aadeaf7426a96ca6669cc9dfdc99ee
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-05-28 15:14:31 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -9,9 +9,9 @@ console.log('Variables de entorno:', {
   BD_PORT: process.env.BD_PORT
 });
 
-export const BD_HOST = process.env.BD_HOST || 'localhost'
-export const BD_DATABASE = process.env.BD_DATABASE || 'base2025'
-export const BD_USER = process.env.BD_USER || 'root'
-export const BD_PASSWORD = process.env.BD_PASSWORD || ''
+export const BD_HOST = process.env.BD_HOST || 'mysql-daniel200423.alwaysdata.net'
+export const BD_DATABASE = process.env.BD_DATABASE || 'daniel200423_1'
+export const BD_USER = process.env.BD_USER || '414160_daniel'
+export const BD_PASSWORD = process.env.BD_PASSWORD || 'D23T07S2004'
 export const BD_PORT = process.env.BD_PORT || 3306
-export const PORT = process.env.PORT || 3000
+export const PORT = process.env.PORT || 10000
