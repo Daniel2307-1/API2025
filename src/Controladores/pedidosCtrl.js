c91bcec51cc0dc5ba77be2d1ec6f7f41ec0c36f6
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-05-29 14:15:43 -0500

    Create pedidosCtrl.js

---
+++ src/Controladores/pedidosCtrl.js
@@ -0,0 +1,46 @@
+import{sql} from '../bd.js'
+export const enviarmensajedebasededatos=(req,res)=>{
+    res.send('Lista de pedidos');
+}
+export const obetenerdatos = async (req, res) => {
+    try {
+        const [result] = await sql.query('SELECT * FROM pedidos');
+        res.json({ cant: result.length, data: result });
+    }catch (error) {
+            console.error('Error al obtener datos:', error);
+            return res.status(500).json({
+                message: "Error en el servidor",
+                error: {
+                    message: error.message,
+                    code: error.code,
+                    stack: error.stack
+                }
+            });
+    }
+    
+}
+export const obtenerpedidosxid=async(req,res)=>{
+    try{
+        const ID=req.params.id
+        const [result]= await sql.query('select * from pedidos where ped_id=? ',[ID])
+        if(result.length<=0) return res.status(400).json({
+            cli_id: 0,
+            message: "pedido no encontrado"
+        })    
+        res.json(result[0])
+    }catch{
+        return res.status(500).json({message: "Error en el servidor"})
+    }
+}
+export const postPedido = async (req, res) => {
+  try {
+    const { cli_id, ped_fecha, usr_id, ped_estado } = req.body;
+    const [result] = await conmysql.query(
+      'INSERT INTO pedidos (cli_id, ped_fecha, usr_id, ped_estado) VALUES (?, ?, ?, ?)' ,
+      [cli_id, ped_fecha, usr_id, ped_estado]
+    );
+    res.status(201).json({ id: result.insertId, message: "Creado correctamente" });
+  } catch (error) {
+    return res.status(500).json({ message: "Error en el servidor" });
+  }
+};
