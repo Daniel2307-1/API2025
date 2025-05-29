be816368dd52fa3add0f0d66f04b426f4c69804f
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-05-29 14:55:13 -0500

    Update pedidodetalleCtrl.js

--- src/Controladores/pedidodetalleCtrl.js
+++ src/Controladores/pedidodetalleCtrl.js
@@ -14,3 +14,19 @@ export const postDetalle = async (req, res) => {
     return res.status(500).json({ message: "Error en el servidor" });
   }
 };
+export const obetenerdatos = async (req, res) => {
+    try {
+        const [result] = await sql.query('SELECT * FROM pedidos_detalle');
+        res.json({ cant: result.length, data: result });
+    }catch (error) {
+            console.error('Error al obtener datos:', error);
+            return res.status(500).json({
+                message: "Error en el servidor",
+                error: {
+                    message: error.message,
+                    code: error.code,
+                    stack: error.stack
+                }
+            });
+    }
+}
