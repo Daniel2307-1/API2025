cf01647f791b350e040af8a6d2d905b06a84d25a
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-05-29 14:52:37 -0500

    Update pedidodetalle.routes.js

--- src/Routes/pedidodetalle.routes.js
+++ src/Routes/pedidodetalle.routes.js
@@ -2,6 +2,6 @@ import express from 'express';
 import {postDetalle} from '../Controladores/pedidodetalleCtrl.js'
 
 const router = express.Router();
-router.post('/detalle/',postPedido)
+router.post('/detalle/',postDetalle)
 
 export default router
