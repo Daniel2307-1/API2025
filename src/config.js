ba6d98f90241362ed790a5a9edae3607f092b78d
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-05-28 23:31:37 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -14,4 +14,4 @@ export const BD_DATABASE = process.env.BD_DATABASE || 'daniel200423_1'
 export const BD_USER = process.env.BD_USER || '414160_daniel'
 export const BD_PASSWORD = process.env.BD_PASSWORD || 'D23T07S2004'
 export const BD_PORT = process.env.BD_PORT || 3306
-export const PORT = process.env.PORT || 10000
+export const PORT = process.env.PORT || 30000
