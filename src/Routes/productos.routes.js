eecd1d804d52bee949c3e06fd6e77747b68a8b37
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-01 00:43:47 -0500

    Update productos.routes.js

--- src/Routes/productos.routes.js
+++ src/Routes/productos.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import {postProducto,putProducto  } from '../Controladores/productosCtrl.js'
+import {postProducto,putProducto,deleteProductos} from '../Controladores/productosCtrl.js'
 
 const router = express.Router();
 router.post('/productosA',postProducto)
 router.put('/productos/:id',putProducto)
+router.put('/productos/:id',putProducto)
+router.delete('/productosD/:id',deleteProductos)
 
 export default router
