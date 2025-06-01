cbbf31c3d38351e983517bc649d927a0784da41b
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-01 14:56:12 -0500

    Update usuariosCtrl.js

--- src/Controladores/usuariosCtrl.js
+++ src/Controladores/usuariosCtrl.js
@@ -4,20 +4,25 @@ export const enviarmensajedebasededatos = (req, res) => {
     res.send('Lista de usuarios');
 };
 export const obetenerdatosA = async (req, res) => {
-    try {
-        const [result] = await sql.query('SELECT * FROM usuarios WHERE usuario = ? AND clave = ?');
-        res.json({ cant: result.length, data: result });
-    } catch (error) {
-        console.error('Error al obtener datos:', error);
-        return res.status(500).json({
-            message: "Error en el servidor",
-            error: {
-                message: error.message,
-                code: error.code,
-                stack: error.stack
-            }
-        });
-    }
+  const { usuario, clave } = req.params;
+
+  try {
+    const [result] = await sql.query(
+      'SELECT * FROM usuarios WHERE usuario = ? AND clave = ?',
+      [usuario, clave]
+    );
+    res.json({ cant: result.length, data: result });
+  } catch (error) {
+    console.error('Error al obtener datos:', error);
+    return res.status(500).json({
+      message: "Error en el servidor",
+      error: {
+        message: error.message,
+        code: error.code,
+        stack: error.stack
+      }
+    });
+  }
 };
 export const obetenerdatos = async (req, res) => {
     try {
