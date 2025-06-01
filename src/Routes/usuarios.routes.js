f5ab08829ce3df983f6c60bd4485021875c3b77d
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-01 18:31:17 -0500

    Update usuarios.routes.js

--- src/Routes/usuarios.routes.js
+++ src/Routes/usuarios.routes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import {obetenerdatosA,obetenerdatos} from '../Controladores/usuariosCtrl.js'
+import {obetenerdatosA,obetenerdatos,postUsuarios } from '../Controladores/usuariosCtrl.js'
 
 const router = express.Router();
 router.get('/usuariosA/:usuario/:clave', obetenerdatosA);
 router.get('/usuarios',obetenerdatos)
-
+router.post('/usuariosG/',postUsuarios)
 
 export default router;
