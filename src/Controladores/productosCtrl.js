09b6a70b1e050a3f63440df810d3d392bae5725a
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-05-31 22:51:24 -0500

    Update productosCtrl.js

--- src/Controladores/productosCtrl.js
+++ src/Controladores/productosCtrl.js
@@ -18,3 +18,35 @@ export const postProducto = async (req, res) => {
     res.status(500).json({ message: "Error en el servidor" });
   }
 };
+export const putProducto = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      prod_codigo,
+      prod_nombre,
+      prod_stock,
+      prod_precio,
+      prod_activo,
+      prod_imagen
+    } = req.body;
+
+    const [result] = await sql.query(
+      `UPDATE productos
+       SET prod_codigo = ?, prod_nombre = ?, prod_stock = ?, prod_precio = ?, prod_activo = ?, prod_imagen = ?
+       WHERE prod_id = ?`,
+      [prod_codigo, prod_nombre, prod_stock, prod_precio, prod_activo, prod_imagen, id]
+    );
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+
+    // Retorna el producto actualizado
+    const [row] = await sql.query('SELECT * FROM productos WHERE prod_id = ?', [id]);
+    res.json(row[0]);
+
+  } catch (error) {
+    console.error("Error al actualizar producto:", error);
+    res.status(500).json({ message: "Error en el servidor" });
+  }
+};
