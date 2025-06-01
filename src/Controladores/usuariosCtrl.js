50de4f96601c25f34b696065a3eea63901ab2ac9
Daniel2307-1 <danielxdtumbaco@gmail.com>
2025-06-01 14:38:09 -0500

    Create usuariosCtrl.js

---
+++ src/Controladores/usuariosCtrl.js
@@ -0,0 +1,37 @@
+import { sql } from '../bd.js';
+
+export const enviarmensajedebasededatos = (req, res) => {
+    res.send('Lista de usuarios');
+};
+export const obetenerdatosA = async (req, res) => {
+    try {
+        const [result] = await sql.query('SELECT * FROM usuarios WHERE usuario = ? AND clave = ?');
+        res.json({ cant: result.length, data: result });
+    } catch (error) {
+        console.error('Error al obtener datos:', error);
+        return res.status(500).json({
+            message: "Error en el servidor",
+            error: {
+                message: error.message,
+                code: error.code,
+                stack: error.stack
+            }
+        });
+    }
+};
+export const obetenerdatos = async (req, res) => {
+    try {
+        const [result] = await sql.query('SELECT * FROM usuarios');
+        res.json({ cant: result.length, data: result });
+    } catch (error) {
+        console.error('Error al obtener datos:', error);
+        return res.status(500).json({
+            message: "Error en el servidor",
+            error: {
+                message: error.message,
+                code: error.code,
+                stack: error.stack
+            }
+        });
+    }
+};
